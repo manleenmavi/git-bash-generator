4a380f2837e4554d37d4a8b29b1b379e91e7e571
manleenmavi <manleenmavi8284@gmail.com>
2023-02-05 18:44:08 -0700

    Init Commit: Setup JavaFx with Maven and libraries.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module me.mvdev.gitbgen {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens me.mvdev.gitbgen to javafx.fxml;
+    exports me.mvdev.gitbgen;
+}
