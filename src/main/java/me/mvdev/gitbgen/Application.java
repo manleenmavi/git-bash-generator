5e1935c2205c3aa6465b2a47c9bfd0a67aaaeb52
manleenmavi <manleenmavi8284@gmail.com>
2023-02-08 17:11:28 -0700

    Setup base design with css, fxml.

--- src/main/java/me/mvdev/gitbgen/Application.java
+++ src/main/java/me/mvdev/gitbgen/Application.java
@@ -10,7 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("application-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 450);
+        scene.getStylesheets().add(Application.class.getResource("application-view.css").toExternalForm());
         stage.setTitle("Generate Git Bash Command");
         stage.setScene(scene);
         stage.show();
