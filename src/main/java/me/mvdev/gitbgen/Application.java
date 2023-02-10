6b52d5fd0e2b334361d22c61df012da4d0057870
manleenmavi <manleenmavi8284@gmail.com>
2023-02-09 18:23:26 -0700

    Implemented base design, icons, and fonts.

--- src/main/java/me/mvdev/gitbgen/Application.java
+++ src/main/java/me/mvdev/gitbgen/Application.java
@@ -2,6 +2,7 @@ package me.mvdev.gitbgen;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,10 +10,12 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(Application.class.getResourceAsStream("Dongle-Regular.ttf"), 12);
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("application-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 450);
         scene.getStylesheets().add(Application.class.getResource("application-view.css").toExternalForm());
         stage.setTitle("Generate Git Bash Command");
+        stage.getIcons().add(new javafx.scene.image.Image(Application.class.getResourceAsStream("git-bash-gen-icon-2.png")));
         stage.setScene(scene);
         stage.show();
     }
